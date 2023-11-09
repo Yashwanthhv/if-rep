820a26db06ae4a8b41f4ee9723d0d3ffbbfb2317
Yashwanthhv <101575932+Yashwanthhv@users.noreply.github.com>
2023-11-09 14:17:31 +0530

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,3 +1,12 @@
-#!\bin\bash
-read n
-echo "$n"
+#!/bin/bash
+echo "Enter numbers by space:"
+read -a number
+min=${number[0]}
+for i in "${number [@]}"
+do
+if [ "$i" -lt "$min" ];
+then
+min="$i"
+fi
+done
+echo "Smallestnumber is $min"
